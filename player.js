01fb6987654edcab727e816452ddcc4f227de67b
hoanganh2321 <131464214+hoanganh2321@users.noreply.github.com>
2024-05-19 18:38:43 +0700

    Update player.js

--- player.js
+++ player.js
@@ -25,10 +25,10 @@ const { queueNames } = require("./commands/play");
 function initializePlayer(client) {
     const nodes = [
         {
-            host: "lava-v4.ajieblogs.eu.org",
-            password: "https://dsc.gg/ajidevserver",
-            port: 443,
-            secure: true
+            host: "lavalink.oryzen.xyz",
+            password: "oryzen.xyz",
+            port: 80,
+            secure: false
         },
     ];
 
